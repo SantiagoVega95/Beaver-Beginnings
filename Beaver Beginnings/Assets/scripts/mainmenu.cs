71599a6cdebc7c9f5d7eee0d89fc9dade3b15e80
RichardDerkman <45083599+RichardDerkman@users.noreply.github.com>
2021-02-27 13:45:54 -0500

    wow

---
+++ Beaver Beginnings/Assets/scripts/mainmenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class mainmenu : MonoBehaviour
+{
+    // ------------ (BUTTON) Start New Game -----------
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Trainstaion");
+    }
+
+    // ------------ (BUTTON) Exit Game -----------
+    public void ExitGame()
+    {
+        Debug.Log("Exited game");
+        Application.Quit();
+    }
+
+}
