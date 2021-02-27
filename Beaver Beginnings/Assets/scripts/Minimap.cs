d9c6de2efa3fb5404a2914140973bb1ac6108bb3
RichardDerkman <45083599+RichardDerkman@users.noreply.github.com>
2021-02-27 16:24:46 -0500

    love life my mini map

--- Beaver Beginnings/Assets/scripts/Minimap.cs
+++ Beaver Beginnings/Assets/scripts/Minimap.cs
@@ -1,18 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Minimap : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // ------------ (BUTTON) to the Bar -----------
+    public void Bar()
     {
-        
+        SceneManager.LoadScene("BAR");
     }
 
-    // Update is called once per frame
-    void Update()
+    // ------------ (BUTTON) to the Cafe -----------
+    public void cafe()
     {
-        
+       SceneManager.LoadScene("Cafe");
+    }
+
+// ------------ (BUTTON) to the  ---Trainstaion--------
+     public void trainstaion()
+    {
+        SceneManager.LoadScene("Trainstaion");
+    }
+
+    // ------------ (BUTTON) to the Home -----------
+     public void home()
+    {
+        SceneManager.LoadScene("Home");
     }
 }
