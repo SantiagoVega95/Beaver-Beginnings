01cd79c5c8ef0c9f530a22bfd76830a44f6076fd
amcgulapa <75518436+amcgulapa@users.noreply.github.com>
2021-02-28 14:02:39 -0500

    added gym scene, made some scripts, added backgrounds :D

---
+++ Beaver Beginnings/Assets/scripts/CafeScene.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CafeScene : MonoBehaviour
+{
+    int click;
+    public GameObject t1;
+    public GameObject t2;
+    public GameObject t3;
+    public GameObject Speechbox;
+    public GameObject Name;
+    public GameObject Character;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        click = 3;
+        Speechbox.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (click == 2)
+        {
+            t1.SetActive(true);
+            t2.SetActive(false);
+            Speechbox.SetActive(true);
+            Name.SetActive(true);
+        }
+
+        if (click == 1)
+        {
+            t1.SetActive(false);
+            t2.SetActive(true);
+            Speechbox.SetActive(true);
+            Name.SetActive(true);
+        }
+
+        if (click == 0)
+        {
+            SceneManager.LoadScene("Minimap");
+        }
+        if (Input.GetButtonDown("Fire1"))
+        {
+            AddScore();
+        }
+    }
+
+    public void AddScore()
+    {
+        click -= 1;
+    }
+}
