39f70817a276b3e1b4df9ac7028fd2d990740332
RichardDerkman <45083599+RichardDerkman@users.noreply.github.com>
2021-02-28 15:05:39 -0500

    Love me

---
+++ Beaver Beginnings/Assets/scripts/B1.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class B1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    int click;
+
+    void Start()
+    {
+        click = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (click == 0)
+        {
+            SceneManager.LoadScene("mainmenu");
+        }
+        if (Input.GetButtonDown("Fire1"))
+        {
+            AddScore();
+        }
+    }
+
+    public void AddScore()
+    {
+        click -= 1;
+    }
+
+}
